df78db8569a23b11cf30cb66358172a2122cad9b
CharPrings <132065358+CharPrings@users.noreply.github.com>
2024-09-05 14:58:52 +0100

    Updated font, adjusted layout of code, removed one city as standard

--- src/index.js
+++ src/index.js
@@ -18,15 +18,6 @@ let parisTime = moment().tz("Europe/Paris");
 parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
-// London
-let londonElement = document.querySelector("#london");
-if (londonElement) {
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector(".time");
-let londonTime = moment().tz("Europe/London");
-londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
-}
 };
 function updateCity(event) {
     let cityTimeZone = event.target.value;
