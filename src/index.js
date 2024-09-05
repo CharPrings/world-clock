46d54958a5e0c15cf49177682b1fef7b06d770da
CharPrings <132065358+CharPrings@users.noreply.github.com>
2024-09-05 13:29:54 +0100

    Added moment.js tz and JS to update time and date

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function updateTime() {
+
+// Tokyo
+let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+// Paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+// London
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment().tz("Europe/London");
+londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+};
+
+updateTime();
+setInterval(updateTime, 1000);
